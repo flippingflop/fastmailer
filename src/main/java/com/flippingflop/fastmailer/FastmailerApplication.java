dab93a65212c7a7b956ca3c1021e6a2c71175925
flippingflop <dreamyscott@naver.com>
2023-11-22 12:27:17 +0900

    initial commit

---
+++ src/main/java/com/flippingflop/fastmailer/FastmailerApplication.java
@@ -0,0 +1,13 @@
+package com.flippingflop.fastmailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastmailerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastmailerApplication.class, args);
+    }
+
+}
