75d37c6ac6ae364a5894a62fda8bb48833dffe83
flippingflop <dreamyscott@naver.com>
2023-11-29 17:50:10 +0900

    Fix TemplateVariable to save CreatedDate

--- src/main/java/com/flippingflop/fastmailer/model/vo/TemplateVariable.java
+++ src/main/java/com/flippingflop/fastmailer/model/vo/TemplateVariable.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "template_variable", indexes = {
         @Index(name = "IDX_KEY_NAME", columnList = "KEY_NAME")
 })
